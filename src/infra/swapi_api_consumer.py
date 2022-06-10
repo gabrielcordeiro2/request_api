9afb0bd5911c56e996fc87dd67b58234c4fb85ca
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-06-09 21:38:48 -0300

    test: testing mock http request

--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -1,10 +1,14 @@
 import requests
 
 class SwapiApiConsumer:
-
+    '''
+        Teste
+    '''
     @classmethod
-    def get_starships(self, page: int) -> any:
-
+    def get_starships(cls, page: int) -> any:
+        '''
+            Texto
+        '''
         params = {'page': page}
         response = requests.get('https://swapi.dev/api/starships/', params=params)
 
